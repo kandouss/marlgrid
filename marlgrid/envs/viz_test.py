85cccae9a877dd0a16399c33a555995dba7a8f86
Kamal <kamal.ndousse@gmail.com>
2020-03-05 12:58:16 -0800

    Change envs/ structure to mirror minigrid

---
+++ marlgrid/envs/viz_test.py
@@ -0,0 +1,17 @@
+from .base import (
+    MultiGridEnv,
+    MultiGrid
+)
+from .objects import *
+
+
+class VisibilityTestEnv(MultiGridEnv):
+    mission='get to the green square'
+    metadata={}
+
+    def _gen_grid(self, width, height):
+        self.grid = MultiGrid((width, height))
+        self.grid.wall_rect(0, 0, width, height)
+        self.grid.horz_wall(0, height//2, width-3, obj_type=Wall)
+
+        self.place_agents()
