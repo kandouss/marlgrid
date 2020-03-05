b49b5eae924dc1188f6e76104fe8d12c713d8472
Kamal <kamal.ndousse@gmail.com>
2020-03-05 12:35:00 -0800

    fix, rename video_test.py

--- examples/video_test.py
+++ examples/video_test.py
@@ -3,16 +3,18 @@ import gym_minigrid
 
 env = gym_minigrid.envs.empty.EmptyEnv(size=10)
 env.max_steps = 200
-env = GridRecorder(env, tile_size=11)
+
+env = GridRecorder(env, render_kwargs={'tile_size':11})
 
 obs = env.reset()
+env.recording = True
 
 count=0
 done = False
+
 while not done:
     act = env.action_space.sample()
     obs, rew, done, _ = env.step(act)
     count += 1
 
-print(f"Done after {count} frames!")
-env.export_video('test.mp4', progress_bar=True, progress_height=7)
+env.export_video('test_minigrid.mp4')
