e183fe81431f7c7a22e603782b1ac38265925283
Kamal <kamal.ndousse@gmail.com>
2020-05-29 23:16:42 -0700

    Version bump!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="marlgrid",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=["numpy", "tqdm", "gym", "gym-minigrid"],
 )
