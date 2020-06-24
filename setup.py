21d854935f7df15b28e2ae5e8042c2b36d7e6c7a
Kamal <kamal.ndousse@gmail.com>
2020-06-23 19:34:07 -0700

    Update setup.py to include numba requirement.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="marlgrid",
     version="0.0.3",
     packages=find_packages(),
-    install_requires=["numpy", "tqdm", "gym", "gym-minigrid"],
+    install_requires=["numpy", "tqdm", "gym", "gym-minigrid", "numba"],
 )
