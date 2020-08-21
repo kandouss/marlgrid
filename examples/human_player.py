692024d937ad62240b653caa1c5bfd08403e7f75
Kamal <kamal.ndousse@gmail.com>
2020-08-21 12:46:51 -0700

    Add example with human player.

---
+++ examples/human_player.py
@@ -0,0 +1,87 @@
+import numpy as np
+import marlgrid
+
+from marlgrid.rendering import InteractivePlayerWindow
+from marlgrid.agents import GridAgentInterface
+from marlgrid.envs import env_from_config
+
+class HumanPlayer:
+    def __init__(self):
+        self.player_window = InteractivePlayerWindow(
+            caption="interactive marlgrid"
+        )
+        self.episode_count = 0
+
+    def action_step(self, obs):
+        return self.player_window.get_action(obs.astype(np.uint8))
+
+    def save_step(self, obs, act, rew, done):
+        print(f"   step {self.step_count:<4d}: reward {rew} (episode total {self.cumulative_reward})")
+        self.cumulative_reward += rew
+        self.step_count += 1
+
+    def start_episode(self):
+        self.cumulative_reward = 0
+        self.step_count = 0
+    
+    def end_episode(self):
+        print(
+            f"Finished episode {episode_count} after {self.step_count} steps."
+            f"  Episode return was {self.cumulative_reward}."
+        )
+        self.episode_count += 1
+
+
+env_config =  {
+    "env_class": "ClutteredGoalCycleEnv",
+    "grid_size": 13,
+    "max_steps": 250,
+    "clutter_density": 0.15,
+    "respawn": True,
+    "ghost_mode": True,
+    "reward_decay": False,
+    "n_bonus_tiles": 3,
+    "initial_reward": True,
+    "penalty": -1.5
+}
+
+player_interface_config = {
+    "view_size": 9,
+    "view_offset": 2,
+    "view_tile_size": 15,
+    "observation_style": "rich",
+    "see_through_walls": False,
+    "color": "prestige"
+}
+
+# Add the agent configuration to the list of agents.
+env_config['agents'] = [player_interface_config]
+
+# Create the environment based on the combined env/player config
+env = env_from_config(env_config)
+
+# Create a human player interface per the class defined above
+human = HumanPlayer()
+
+# Start an episode!
+# Each observation from the environment contains a list of observaitons for each agent.
+# In this case there's only one agent so the list will be of length one.
+obs_list = env.reset()
+
+human.start_episode()
+done = False
+while not done:
+
+    player_action = human.action_step(obs_list[0]['pov'])
+    # The environment expects a list of actions, so put the player action into a list
+    agent_actions = [player_action]
+
+    next_obs_list, rew_list, done, _ = env.step(agent_actions)
+    
+    human.save_step(
+        obs_list[0], player_action, rew_list[0], done
+    )
+
+    obs_list = next_obs_list
+
+human.end_episode()
--- marlgrid/rendering.py
+++ marlgrid/rendering.py
@@ -33,18 +33,24 @@ class SimpleImageViewer(object):
                 self.isopen = False
 
         assert len(arr.shape) == 3, "You passed in an image with the wrong number shape"
+
         image = pyglet.image.ImageData(arr.shape[1], arr.shape[0],
             'RGB', arr.tobytes(), pitch=arr.shape[1]*-3)
         gl.glTexParameteri(gl.GL_TEXTURE_2D,
             gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
         texture = image.get_texture()
-        texture.width = self.width
-        texture.height = self.height
+
+        aspect_ratio = arr.shape[1]/arr.shape[0]
+        forced_width = min(self.width, self.height * aspect_ratio)
+        texture.height = int(forced_width / aspect_ratio)
+        texture.width = int(forced_width)
+
         self.window.clear()
         self.window.switch_to()
         self.window.dispatch_events()
         texture.blit(0, 0) # draw
         self.window.flip()
+        
     def close(self):
         if self.isopen and sys.meta_path:
             # ^^^ check sys.meta_path to avoid 'ImportError: sys.meta_path is None, Python is likely shutting down'
@@ -53,3 +59,42 @@ class SimpleImageViewer(object):
 
     def __del__(self):
         self.close()
+
+
+class InteractivePlayerWindow(SimpleImageViewer):
+    def __init__(self, display=None, caption=None, maxwidth=500):
+        super().__init__(display=display, caption=caption, maxwidth=maxwidth)
+        self.key = None
+        self.action_count = 0
+
+        self.action_map = {
+            pyglet.window.key._0:0,
+            pyglet.window.key._1:1,
+            pyglet.window.key._2:2,
+            pyglet.window.key._3:3,
+            pyglet.window.key._4:4,
+            pyglet.window.key._5:5,
+            pyglet.window.key._6:6,
+            pyglet.window.key.LEFT:0,
+            pyglet.window.key.RIGHT:1,
+            pyglet.window.key.UP:2,
+            # pyglet.window.key.Q:-1,
+        }
+
+    def get_action(self, obs):
+        if self.window is None:
+            self.imshow(obs)
+
+            @self.window.event
+            def on_key_press(symbol, modifiers):
+                self.key = symbol
+
+            return self.get_action(obs)
+    
+        self.imshow(obs)
+        self.key = None
+        while self.key not in self.action_map:
+            self.window.dispatch_events()
+            pyglet.clock.tick()
+
+        return self.action_map[self.key]
